6127e2198ee1f9da89cab773fc3caaac6be58ecd
basteln3rk <gnutzmann@flexponsive.net>
2023-11-05 23:17:44 +0000

    generated types without eloquentmodeltransformer

--- resources/types/generated.d.ts
+++ resources/types/generated.d.ts
@@ -1,38 +1,38 @@
 declare namespace App.Enums {
-export type UserRole = 'admin' | 'guest' | 'professional';
+    export type UserRole = "admin" | "guest" | "professional";
 }
 declare namespace App.Models {
-export type OrgUnit = {
-id: number;
-name: string;
-address?: string;
-created_at?: string;
-updated_at?: string;
-};
-export type User = {
-id: number;
-name: string;
-email: string;
-email_verified_at?: string;
-created_at?: string;
-updated_at?: string;
-role?: App.Enums.UserRole;
-bigint_without_cast?: number;
-names_of_siblings?: Array<any>;
-secret_question?: string;
-is_active: boolean;
-cohort_month?: string;
-signup_fee?: string;
-rand_double?: number;
-secret_answer?: string;
-rand_float?: number;
-options?: object;
-login_count?: number;
-last_login_ts?: number;
-org_unit_id?: number;
-reverse_email: any;
-orgUnit?: App.Models.OrgUnit;
-tokens?: Array<any>;
-notifications?: Array<any>;
-};
+    export type BusinessFunction = {
+        incrementing: boolean;
+        preventsLazyLoading: boolean;
+        exists: boolean;
+        wasRecentlyCreated: boolean;
+        timestamps: boolean;
+        usesUniqueIds: boolean;
+    };
+    export type BusinessFunctionOrgUnit = {
+        incrementing: boolean;
+        preventsLazyLoading: boolean;
+        exists: boolean;
+        wasRecentlyCreated: boolean;
+        timestamps: boolean;
+        usesUniqueIds: boolean;
+        pivotParent: any;
+    };
+    export type OrgUnit = {
+        incrementing: boolean;
+        preventsLazyLoading: boolean;
+        exists: boolean;
+        wasRecentlyCreated: boolean;
+        timestamps: boolean;
+        usesUniqueIds: boolean;
+    };
+    export type User = {
+        incrementing: boolean;
+        preventsLazyLoading: boolean;
+        exists: boolean;
+        wasRecentlyCreated: boolean;
+        timestamps: boolean;
+        usesUniqueIds: boolean;
+    };
 }
