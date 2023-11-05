29d4f47d32fb96010b88af1b013951c2f07f799f
basteln3rk <gnutzmann@flexponsive.net>
2023-11-05 23:18:43 +0000

    generated types with eloquentmodeltransformer

--- resources/types/generated.d.ts
+++ resources/types/generated.d.ts
@@ -3,36 +3,61 @@ declare namespace App.Enums {
 }
 declare namespace App.Models {
     export type BusinessFunction = {
-        incrementing: boolean;
-        preventsLazyLoading: boolean;
-        exists: boolean;
-        wasRecentlyCreated: boolean;
-        timestamps: boolean;
-        usesUniqueIds: boolean;
+        id: number;
+        name: string;
+        description?: string;
+        created_at?: string;
+        updated_at?: string;
+        orgUnits?: Array<
+            App.Models.OrgUnit & {
+                contribution?: App.Models.BusinessFunctionOrgUnit;
+            }
+        >;
     };
     export type BusinessFunctionOrgUnit = {
-        incrementing: boolean;
-        preventsLazyLoading: boolean;
-        exists: boolean;
-        wasRecentlyCreated: boolean;
-        timestamps: boolean;
-        usesUniqueIds: boolean;
-        pivotParent: any;
+        id: number;
+        business_function_id: number;
+        org_unit_id: number;
+        created_at?: string;
+        updated_at?: string;
     };
     export type OrgUnit = {
-        incrementing: boolean;
-        preventsLazyLoading: boolean;
-        exists: boolean;
-        wasRecentlyCreated: boolean;
-        timestamps: boolean;
-        usesUniqueIds: boolean;
+        id: number;
+        name: string;
+        address?: string;
+        created_at?: string;
+        updated_at?: string;
+        users?: Array<App.Models.User>;
+        businessFunctions?: Array<
+            App.Models.BusinessFunction & {
+                pivot?: App.Models.BusinessFunctionOrgUnit;
+            }
+        >;
     };
     export type User = {
-        incrementing: boolean;
-        preventsLazyLoading: boolean;
-        exists: boolean;
-        wasRecentlyCreated: boolean;
-        timestamps: boolean;
-        usesUniqueIds: boolean;
+        id: number;
+        name: string;
+        email: string;
+        email_verified_at?: string;
+        created_at?: string;
+        updated_at?: string;
+        role?: App.Enums.UserRole;
+        bigint_without_cast?: number;
+        names_of_siblings?: Array<any>;
+        secret_question?: string;
+        is_active: boolean;
+        cohort_month?: string;
+        signup_fee?: string;
+        rand_double?: number;
+        secret_answer?: string;
+        rand_float?: number;
+        options?: object;
+        login_count?: number;
+        last_login_ts?: number;
+        org_unit_id?: number;
+        reverse_email: any;
+        orgUnit?: App.Models.OrgUnit;
+        tokens?: Array<any>;
+        notifications?: Array<any>;
     };
 }
